251d0fd92a7a828583ca02f8bd3234d4302b4c98
Shwetalbhargav <hridaan.purav@gmail.com>
2025-09-15 15:57:56 +0530

    added promise

--- src/app/buyers/[id]/page.tsx
+++ src/app/buyers/[id]/page.tsx
@@ -3,8 +3,14 @@ import BuyerForm from "@/components/buyers/BuyerForm";
 import HistoryList from "@/components/buyers/HistoryList";
 import GuardOwner from "@/components/buyers/GuardOwner";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const buyer = await getBuyer(params.id);
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const buyer = await getBuyer(id);
 
   return (
     <div className="grid lg:grid-cols-3 gap-6">
